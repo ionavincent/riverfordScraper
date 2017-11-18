095324706381a22c225e66cd50541e7e6ab156b8
Iona Vincent <ionagvincent@gmail.com>
2017-11-18 15:49:56 +0000

    add tests

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,2 +1,49 @@
-def test():
-    assert 1 == 1
+import json
+import os
+import unittest
+
+from riverfordScraper import cabbageCheck, parser
+
+
+class TestScraper(unittest.TestCase):
+    def setUp(self):
+        self.allowedBoxes = ["Large veg box (less roots)",
+                             "Large veg box",
+                             "Bumper veg box (original)"]
+
+        with open(os.path.join(os.path.dirname(__file__),
+                               "data/scrapedHtml.html"), "r") as f:
+            self.testHtml = f.read()
+
+        with open(os.path.join(os.path.dirname(__file__),
+                               "data/boxContents.json"), "r") as f:
+            self.sampleBoxContents = json.load(f)
+
+    def testParser(self):
+        output = parser.parseBoxContentsPage(self.testHtml)
+        self.assertEqual(self.sampleBoxContents, output,
+                         "Wrong box contents extracted")
+
+    def testContainsCabbage(self):
+        self.assertFalse(cabbageCheck.containsCababage(
+                        self.sampleBoxContents["Juicing box"]),
+                        "Found invisible cabbage")
+
+        self.assertTrue(cabbageCheck.containsCababage(
+                        self.sampleBoxContents["Bumper veg box (original)"]),
+                        "Failed to spot cabbage")
+
+    def testfindAlternativeBoxes(self):
+        self.assertEqual(cabbageCheck.findAlternativeBoxes(
+                                                    self.sampleBoxContents,
+                                                    self.allowedBoxes),
+                         [])
+        self.assertEqual(cabbageCheck.findAlternativeBoxes(
+                                                    self.sampleBoxContents,
+                                                    ["Salad box",
+                                                     "100% UK veg box"]),
+                         ["Salad box"])
+
+
+if __name__ == '__main__':
+    unittest.main()
