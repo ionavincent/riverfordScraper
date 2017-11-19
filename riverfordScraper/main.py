db66a0e51119b032096ab791a705c80115503775
Iona Vincent <ionagvincent@gmail.com>
2017-11-19 17:09:46 +0000

    add slack notifications

--- riverfordScraper/main.py
+++ riverfordScraper/main.py
@@ -1,35 +1,39 @@
 import parser
+import sched
 
 import cabbageCheck
+import config
+import notifier
 from scraper import Scraper
-
-URL = "https://www.riverford.co.uk/box-contents"
-PREFERRED_BOX = "Large veg box (less roots)"
-ALLOWED_BOXES = ["Large veg box (less roots)",
-                 "Large veg box",
-                 "Bumper veg box (original)"]
 
 
 def main():
     scraper = Scraper(cache_name=__name__, cache_expiry=36000)
-    scrapedHtml = scraper.get(url=URL)
+    scrapedHtml = scraper.get(url=config.riverfordUrl)
 
     allBoxesInfo = parser.parseBoxContentsPage(scrapedHtml)
-    if cabbageCheck.containsCababage(allBoxesInfo[PREFERRED_BOX]):
+
+    message = getMessage(allBoxesInfo)
+    if message:
+        notifier.sendSlackNotification(message)
+
+
+def getMessage(allBoxesInfo):
+    if cabbageCheck.containsCababage(allBoxesInfo[config.preferredBox]):
         altBoxes = cabbageCheck.findAlternativeBoxes(allBoxesInfo,
-                                                     ALLOWED_BOXES)
+                                                     config.allowedBoxes)
         if len(altBoxes):
-            print ("Your preferred box has cabbage in it this week. "
+            return ("Your preferred box has cabbage in it this week. "
                    "But fear not, there are "
                    "cabbage free alternatives: %s" % ', '.join(altBoxes))
         else:
-            print "Alas, all the boxes you buy contain cabbage this week"
+            return ("Alas, all the boxes you buy contain cabbage this week. "
+                    "Bad luck.")
     else:
-        print "You are free from cabbage this week"
+        return "You are free from cabbage this week"
 
 
 if __name__ == '__main__':
     main()
 
-# TODO: Add slack notifications
 # TODO: Add config file
---
+++ riverfordScraper/notifier.py
@@ -0,0 +1,17 @@
+import json
+import requests
+
+import config
+
+def sendSlackNotification(text):
+    slack_data = {'text': "%s" % text}
+
+    response = requests.post(
+        config.slackUrl, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
+    )
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
